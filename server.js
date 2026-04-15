85c30e0ff225a7bf38152112dff5ffc40f3e244f
Advith Shetty <advithshetty@Adviths-MacBook-Air.local>
2026-04-16 01:52:57 +0530

    fix port for render deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const Blockchain = require('./blockchain');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
